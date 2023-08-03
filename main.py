658eb63d0b36b158c86ed18eee51cdcb2251117f
Alexander Deryabin <71517115+saskedr@users.noreply.github.com>
2023-08-03 19:12:33 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
-d = {'name': 'alex', 'age': 10}
+import discord
+from discord.ext import commands
 
-print(d['age'])
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Привет! Я бот {bot.user}!')
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+bot.run("MTEzNDE1M****")
